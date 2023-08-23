fe1a1b6e0086a6b84d8fd5b6094eb697dd97a5ba
Eduardo Rendón <eduardorendon99@gmail.com>
2023-08-23 14:19:19 -0600

    Agregando segunda parte del proyecto integrador

--- functions.js
+++ functions.js
@@ -1,14 +1,41 @@
-const listaContactos = ['Maria Lopez', 'Juan Perez', 'Carlos Sanchez', 'Daniela Hernandez'];
-listaContactos.sort();
-console.log(listaContactos);
+const listaContactos = [
+    {
+        "id" : "398",
+        "nombre" : "Juan",
+        "apellido" : "Perez",
+        "telefono" : "99887766",
+        "ubicaciones" : {
+            "ciudad" : "Tangamandapio",
+            "direccion" : "Barrio Los Andes",
+        },
+    },
+    {
+        "id" : "486",
+        "nombre" : "Maria",
+        "apellido" : "Lopez",
+        "telefono" : "33554411",
+        "ubicaciones" : {
+            "ciudad" : "Wakanda",
+            "direccion" : "Colonia Loma Linda",
+        },
+    },
+];
+
+//listaContactos.sort();
+//console.log(listaContactos);
+//console.log(listaContactos[1].id);
 
 function agregar()
 {
-    let contacto = prompt('Agregar contacto:');
+    let id = prompt('Agregar id de contacto:');
+    let nombre = prompt('Agregar nombre de contacto:');
+    let apellido = prompt('Agregar apellido de contacto:');
+    let telefono = prompt('Agregar telefono de contacto:');
+    let ciudad = prompt('Agregar ciudad de contacto:');
+    let direccion = prompt('Agregar direccion de contacto:');
     let existe = '';
     for (var i = 0; i < listaContactos.length; i++) {
-        
-        if (contacto == listaContactos[i]) {
+        if (id == listaContactos[i].id) {
             existe = 'si';
             break;
         }
@@ -17,24 +44,34 @@ function agregar()
         }
     }
     if(existe == 'si'){
-        alert('Contacto ya existe');
+        alert('Contacto ya existe, no puede usarse el mismo identificador.');
     }
     else{
-        listaContactos.push(contacto);
+        let objeto = {
+            "id" : id,
+            "nombre" : nombre,
+            "apellido" : apellido,
+            "telefono" : telefono,
+            "ubicaciones" : {
+                "ciudad" : ciudad,
+                "direccion" : direccion,
+            },
+        };
+        listaContactos.push(objeto);
         listaContactos.sort();
         alert('Se agregó el contacto');
     }
     
-    console.log(listaContactos);
+    //console.log(listaContactos);
 }
 
 function borrar(){
-    let contacto = prompt('Borrar contacto:');
+    let id = prompt('Borrar contacto mediante id:');
     let existe = '';
     let index = '';
     for (var i = 0; i < listaContactos.length; i++) {
         
-        if (contacto == listaContactos[i]) {
+        if (id == listaContactos[i].id) {
             existe = 'si';
             index = i;
             break;
@@ -52,21 +89,21 @@ function borrar(){
         alert('Se borró el contacto');
     }
     
-    console.log(listaContactos);
+    //console.log(listaContactos);
 }
 
 function imprimir(){
     let i = '';
     listaContactos.forEach(function (elemento, indice, array) {
-        console.log(indice+1+' - '+elemento);
+        console.table(indice+1+' - '+JSON.stringify(elemento));
         i++;
         let last = listaContactos.length;
         if(i<last){
-            console.log('---------------------------');
+            console.log('--------------------------------------------------------------------------------------');
         }
       });
+      listaContactos.sort();
 }
 
-agregar();
-borrar();
+
 imprimir();
